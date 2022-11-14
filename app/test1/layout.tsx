0bc0d7422a3955ea67d6f6f9ccd5d93c5d061089
pokuwagata <pokuwagata@gmail.com>
2022-11-15 08:24:45 +0900

    add pages/index.tsx

--- app/test1/layout.tsx
+++ app/test1/layout.tsx
@@ -1,4 +1,4 @@
-import { prisma } from "../src/server/db/client";
+import { prisma } from "../../src/server/db/client";
 
 // export const revalidate = 10;
 
---
+++ src/pages/index.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div>
+      <p>pages/index.tsx</p>
+    </div>
+  );
+}
